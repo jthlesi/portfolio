2a98116d0b0e43b883a12313580f2ea24e5fb8d8
jthlesi <distanc87@gmail.com>
2022-08-08 18:26:41 +0900

    0808

--- js/index.js
+++ js/index.js
@@ -1,26 +1,52 @@
+var scrollCount = 0;
+function move(page) {
+    scrollCount = page;
+    console.log(scrollCount)
+}
 $(window).on("scroll mousewheel", function(event){
-    var page=1;
-    console.log(page)
+
     if (event.originalEvent.wheelDelta >= 0) {
-        if (page==1){
-            $("#cube_2").stop().animate({"width":"50vh","height":"50vh","left":"0","top":"0"});
-            $("#text_wrap").fadeIn("slow")    
+        move(scrollCount-1);
+        if (scrollCount<0){
+            scrollCount=0;
+        }
+        if (scrollCount==0){
+            $("#introduce").animate({"margin-top":"100vh"});
+            setTimeout(function(){
+                $("#cube_2").stop().animate({"width":"50vh","height":"50vh","left":"0","top":"0"});
+            },800);
+            setTimeout(function(){
+                for (i=0;i<20;i++){
+                    $("#text_wrap h1 span").eq(i).css({"animation":"text_out_"+i+"_back 0.5s linear both"});
+                }
+                $("#text_wrap").show()
+            },1800);
             setTimeout(function(){
                 $("#cube").css({"animation":"spin 8s linear infinite"});
-
-            },1000)
+            },2300);
         }
+
     }else{
-        if (page==1){
+        move(scrollCount+1)
+        if (scrollCount==1){
             var calc= (-$(window).width()/2 + $(window).height()/4)
-            $("#cube").css({"animation":"spin_back 0.5s linear"});
-            setTimeout(function(){
-                $("#text_wrap").fadeOut("slow")
-                $("#cube_2").stop().animate({"width":"100vw","height":"100vh","left":calc,"top":"-25vh"});
-            },1000)
-            page++;
-        }
 
-}
+            for (i=0;i<20;i++){
+                $("#text_wrap h1 span").eq(i).css({"animation":"text_out_"+i+" 0.5s linear both"});
+            }
+            setTimeout(function(){
+            $("#cube").css({"animation":"spin_back 0.5s linear"});
+            },500);
+            setTimeout(function(){
+                $("#text_wrap").hide()
+                $("#cube_2").stop().animate({"width":"100vw","height":"100vh","left":calc,"top":"-25vh"});
+            },1500);
+            setTimeout(function(){
+                $("#introduce").animate({"margin-top":"-100vh"});
+
+            },2300)
+        } else if (scrollCount==2){
+        }
+    }
 })
 
